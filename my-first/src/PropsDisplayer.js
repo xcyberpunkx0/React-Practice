ec6399fe07e865db368b49fd8bca8c859b5e0569
xcyberpunkx0 <xcyberpunkx0@gmail.com>
2024-09-07 13:29:45 +0530

    First Prop Practice

---
+++ my-first/src/PropsDisplayer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PropsDisplayer(props) {
+  const stringProps = JSON.stringify(props);
+  return (
+    <div>
+      <h1>CHECK OUT MY PROPS OBJECT</h1>
+      <h2>{stringProps}</h2>
+    </div>
+  );
+}
+
+export default PropsDisplayer;
