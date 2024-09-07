7d59fdcfd7161169e81cbf865222d705a33459a8
xcyberpunkx0 <xcyberpunkx0@gmail.com>
2024-09-07 13:49:16 +0530

    Passing Props from App to Product

--- my-first/src/App.js
+++ my-first/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Product from './Product'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Product name="Apple Watch" price = {399} rating = "4.5/5.0" />;
 }
 
 export default App;
---
+++ my-first/src/Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Product(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.price}</h2>
+      <h3>{props.rating}</h3>
+    </div>       
+  );
+}
+
+export default Product;
